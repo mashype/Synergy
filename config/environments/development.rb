597fc1dda3fc75bac1f140aa94236f7d75ba9076
mashype <mashype@me.com>
2016-07-04 15:00:51 -0700

    testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #needed for devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+
 end
